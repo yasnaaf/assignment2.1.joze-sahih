aa4ae9a0272fd22d1fe97de22ede0eb21551bac6
yasnaaf <152720689+yasnaaf@users.noreply.github.com>
2023-12-23 18:28:34 +0330

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+float x ;
+printf("Enter the number:");
+scanf ("%f",&x);
+int integer_part;
+integer_part = x ;
+printf("integer_part=%d",integer_part);
+return 0;
+}
